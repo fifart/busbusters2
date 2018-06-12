da21955bb4d2cb65ccd6d9ee93ba9af4a4b64837
fifart <fifart@gmail.com>
2018-06-12 17:06:23 +0300

    links of categories added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,15 @@ class PagesController < ApplicationController
     @postings = Posting.all.order(created_at: :desc)
     @categories = Category.all
   end
+
+  
+def requests
+    @postings = Posting.all.order(created_at: :desc).where(category_id: 1)
+    @categories = Category.all
+  end
+
+  def declarations
+    @postings = Posting.all.order(created_at: :desc).where(category_id: 2)
+    @categories = Category.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :postings, only: [:show, :update, :destroy, :new, :create, :edit]
   devise_for :users
   root 'pages#index'
+  get '/requests', to: "pages#requests"
+  get '/declarations', to: "pages#declarations"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
